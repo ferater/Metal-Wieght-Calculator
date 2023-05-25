0a5db6607245cce5b43e118abfb8945e1665e5c5
Ferhat ER <lersmooth@gmail.com>
2023-05-25 13:16:55 +0300

    added reset & rectangle weight calc functions

--- js/app.js
+++ js/app.js
@@ -1,23 +1,40 @@
-// select inputs
-const inputs = selectNode("input.rectangle-input", true);
-//select clear button
-const clearButton = selectNode("button.clear");
-//add click event clear button
-clearButton.addEventListener("click", clearAll);
-//select calculate button
-const calculateButton = selectNode("button.calculate");
-//add click event calculate button
-calculateButton.addEventListener("click", calculateWeight);
+//define const intensity
+let intensity = 0;
+
 //define dimensions object
-const RectangleDimensions = {
-  "line-a":0,
-  "line-b":0,
-  "line-h":0,
-}
+const rectangleDimensions = {
+  "rectangle-line-a": 0,
+  "rectangle-line-b": 0,
+  "rectangle-line-h": 0,
+};
+
 //define classNames
 const selectedClassName = "selected";
 const errorClassName = "error";
 const correctClassName = "correct";
+
+//select  mareial select
+const mareialSelect = selectNode("select#material-select");
+//select  mareial intensity text
+const mareialIntensityText = selectNode(".material-intensity");
+//add event material select
+mareialSelect.addEventListener("change", selectMaterial);
+
+// select inputs
+const inputs = selectNode("input.rectangle-input", true);
+
+//select clear button
+const clearButton = selectNode("button.clear");
+//add click event clear button
+clearButton.addEventListener("click", clearAll);
+
+//select calculate button
+const calculateButton = selectNode("button.calculate");
+//add click event calculate button
+calculateButton.addEventListener("click", calculateWeight);
+
+//select show result div
+const showResult = selectNode(".show-result");
 /////////////////////////////////////////
 inputs.forEach((input) => {
   const line = selectNode(`.${input.id}`);
@@ -50,6 +67,7 @@ inputs.forEach((input) => {
       removeClass(line, correctClassName);
       addClass(line, errorClassName);
     } else {
+      removeClass(input, errorClassName);
       removeClass(line, errorClassName);
       addClass(line, correctClassName);
     }
@@ -57,8 +75,18 @@ inputs.forEach((input) => {
   };
 });
 /////////////////////////////////////////
+function selectMaterial() {
+  intensity = mareialSelect.value;
+  mareialIntensityText.innerHTML = `<span>${intensity}</span>`;
+  removeClass(mareialSelect, errorClassName);
+}
+///////////////////////////////////////////
 function clearAll() {
   inputs.forEach((input) => {
+    const line = selectNode(`.${input.id}`);
+    const indicatorText = selectNode(`.indicator-${input.id}`);
+    //remove mareialSelect error class
+    removeClass(mareialSelect, errorClassName);
     //clear input values
     input.value = "";
     //clear input classes
@@ -66,21 +94,51 @@ function clearAll() {
     removeClass(input, errorClassName);
     removeClass(input, correctClassName);
     //clear line classes
-    const line = selectNode(`.${input.id}`)
     removeClass(line, selectedClassName);
     removeClass(line, errorClassName);
     removeClass(line, correctClassName);
     //clear line indicator texts
-    const indicatorText = selectNode(`.indicator-${input.id}`)
-    indicatorText.innerHTML = `<span>0 mm</span>`
-    
+    indicatorText.innerHTML = `<span>0 mm</span>`;
+    //clear show result
+    showResult.innerHTML = `<span>0.00 Kg</span>`;
+    removeClass(showResult, "show");
+  });
+  Object.keys(rectangleDimensions).forEach((key) => {
+    rectangleDimensions[key] = 0;
   });
 }
 /////////////////////////////
-function calculateWeight(){
-  inputs.forEach(input=> {
-    console.log(input.value);
-  })
+function calculateWeight() {
+  if (!intensity) {
+    addClass(mareialSelect, errorClassName);
+    return;
+  }
+  inputs.forEach((input) => {
+    const line = selectNode(`.${input.id}`);
+    const indicatorText = selectNode(`.indicator-${input.id}`);
+    //set rectangleDimensions if input value correct
+    if (input.value && input.value > 0) {
+      rectangleDimensions[input.id] = input.value;
+    } else {
+      //add error class if input value not correct
+      addClass(input, errorClassName);
+      addClass(line, errorClassName);
+      addClass(indicatorText, errorClassName);
+    }
+  });
+  const [a, b, h] = Object.values(rectangleDimensions);
+  if (!a || !b || !h) return;
+
+  const result =
+    (a / 1000) *
+    (b / 1000) *
+    h *
+    intensity;
+
+  if (result > 0) {
+    showResult.innerHTML = `<span>${Number(result.toFixed(2))} Kg</span>`;
+    addClass(showResult, "show");
+  }
 }
 /////////////////////////////////
 //select node(s) function
@@ -98,6 +156,7 @@ function addClass(element, className) {
     element.classList.add(className);
   }
 }
+////////////////////
 //remove class function
 function removeClass(element, className) {
   if (element.classList.contains(className)) {
